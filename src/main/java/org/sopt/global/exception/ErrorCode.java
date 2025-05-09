1bc87467387d11130238a42cbcda43893a12a79f
chounjae <blueskycuj@naver.com>
2025-05-09 17:39:16 +0900

    test#7

--- src/main/java/org/sopt/global/exception/ErrorCode.java
+++ src/main/java/org/sopt/global/exception/ErrorCode.java
@@ -5,7 +5,7 @@ public enum ErrorCode {
     INVALID_CONTENT_LENGTH(400, "게시글이 비어있거나 1000자를 넘습니다.."),
     INVALID_NAME_LENGTH(400, "이름이 비어있거나 10자를 넘습니다.."),
     NULL_HEADER_USERID(400, "헤더에 userId가 없습니다.."),
-    USER_NOT_FOUND(404,  "사용자를 찾을 수 없습니다.."),
+    USER_NOT_FOUND(404,  "사용자를 찾을 수 없습니다..~"),
     POST_NOT_FOUND(404,  "게시글을 찾을 수 없습니다..."),
     BAD_REQUEST(400,  "잘못된 요청입니다.."),
     INTERNAL_SERVER_ERROR(500,  "서버 개발자 잘못입니다..");
